06e37327b18c974e54344a6420d69b3249a92a62
EmanoelDevOase <emanoel.pereira@oase.cloud>
2022-05-01 17:09:14 -0300

    feature-Added Vuex,VueRouter and tailwind to project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h3 class="">
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
